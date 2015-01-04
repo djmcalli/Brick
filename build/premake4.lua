0fd3aebcd0581c2d51f947c95af2cea8d2a07fe7
Dylan McAllister <mcallister.dylan@gmail.com>
2015-01-04 00:40:35 -0500

    To the depends mobile!

---
+++ build/premake4.lua
@@ -0,0 +1,38 @@
+function getAbsoluteFromSolution(p)
+	local sol = solution()
+	return path.getabsolute(path.join(sol.basedir, p))
+end
+
+solution "Feather"
+	configurations { "Debug", "Release" }
+	platforms { "x32", "x64" }
+	flags { "Symbols", "Unicode" }
+	location "projects"
+
+	project "Client"
+		kind "ConsoleApp"
+		language "C++"
+		location "projects"
+		local stargetdir = targetdir "../bin"
+		files { "../src/**" }
+		includedirs { "../src" }
+
+		-- DEPENDENCIES
+		files { "../depend/**" }
+		includedirs { "../depend/headers" } 
+		configuration "x32"
+			libdirs { "../depend/libs-msvc/x86" }
+		configuration "x64"
+			libdirs { "../depend/libs-msvc/x64" }
+
+		-- LINKS
+		configuration { "windows or linux or osx"}
+			links { "SDL2", "SDL2main", "jpeg", "glew" }
+
+		-- Enable C++11 on Linux
+		configuration { "linux or bsd or solaris" }
+			buildoptions { "-std=c++11" }
+
+		-- Per-platform libraries.
+		configuration "windows"
+			links { "ws2_32", "opengl32" }
