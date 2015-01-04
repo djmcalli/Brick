d6d050e99b9f968dd54ae6d7a504632815d0cfbf
Dylan McAllister <mcallister.dylan@gmail.com>
2015-01-04 01:49:53 -0500

    Revert "Revert "Ripped from Callan's repo to get basic setup""
    This reverts commit 249440b0898178d780407102dcc3b5c3a3f2b187.

--- build/premake4.lua
+++ build/premake4.lua
@@ -3,7 +3,7 @@ function getAbsoluteFromSolution(p)
 	return path.getabsolute(path.join(sol.basedir, p))
 end
 
-solution "Feather"
+solution "Brick"
 	configurations { "Debug", "Release" }
 	platforms { "x32", "x64" }
 	flags { "Symbols", "Unicode" }
